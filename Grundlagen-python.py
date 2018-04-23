2a4f455f8b0b2f79e58764a05171e8490a0c1ea6
Montanablack88 <171wita32@o365.htl-leoben.at>
2018-04-23 15:27:33 +0200

    Lieblingszahlen

--- Grundlagen-python.py
+++ Grundlagen-python.py
@@ -20,21 +20,30 @@ if (wer == "ich"):
 else:
     print("Hallo", wer)    
 
-    lieblingszahl = input("Was ist deine Lieblingszahl")
+    lieblingszahl = input("Was ist deine Lieblingszahl? ")
     print("Super, ich mag dei Zahl ", lieblingszahl)
     print("Aber die coolere Zahl ", int(lieblingszahl)+10, " mag ich noch mehr")
 
     runden = input("Wie viele Runden sollen wir Spielen? ")
     runden = int(runden)
-
-    for runde in range(1,runden+1):
-        sieger = ""
-        #zufallszahl erzeugen
-        zufallszahl = random.randint(1,6)
-        #sonst ist der Computer sieger
-        if (zufallszahl == 1 or zufallszahl == 3 or zufallszahl == 5):
-            sieger = "ich"
-            else:
-                sieger = "computer"
-        print("Runde" , runde, "von", runden, ": Sieger :", "ich")
-        print("game over.")
+siege_computer = 0    
+siege_ich = 0
+for runde in range(1,runden+1):
+    sieger = ""
+    #zufallszahl erzeugen
+    zufallszahl = random.randint(1,6)
+    #sonst ist der Computer sieger
+    if (zufallszahl == 1 or zufallszahl == 3 or zufallszahl == 5):
+        sieger = "ich"
+        siege_ich += 1
+    else:
+        siege = "computer"
+        siege_computer += 1
+    print("Runde" , runde, "von", runden, ": Sieger :", sieger, ": gewuerfelt wurde:", zufallszahl)
+if (siege_ich > siege_computer):
+    print("Du gewinnst")
+elif (siege_computer > siege_ich):
+    print("Du verlierst")    
+else:
+     print("Unentschieden")
+print("game over.")        

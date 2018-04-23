6c2caded70f173f18a0f8e5ba872db812a77e9a9
Montanablack88 <171wita32@o365.htl-leoben.at>
2018-04-23 14:01:19 +0200

    Python datei angelegt

--- Grundlagen-python.py
+++ Grundlagen-python.py
@@ -1,14 +1,18 @@
-#Grundlagen-Python.py
+ #Grundlagen-Python.py
+
+# Kommentare efolgen mit Hashtack
+
+# Ausgabe von daten
+
+# Variable definieren (kann ohne Typ erfolgen)
+heimat = "Erde"
 
 
+# Mehrere Varinblen werden mit , getrennt
+print(heimat, "an World: ", "Hallo!")
 
+# Eingabe 
+wer = input("Und wer bist du? ")
 
-
-
-
-
-
-
-
-
-
+# und gibt den text wieder aus
+print ("Hallo ", wer)

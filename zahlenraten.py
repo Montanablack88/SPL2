0891a3c32498bcf017c957b12541dfd7ed76e3ca
Montanablack88 <171wita32@o365.htl-leoben.at>
2018-04-23 16:20:44 +0200

    zahlenratem.py angelegt

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,19 +1,23 @@
 #zahlenraten.py
 
+import random
 max = 200
-zufallszahl = 11
-gewonnen = False
+gewonnen = 0
+Zufallszahl = random.randint(1,max)
 versuche = 0
-print("Zufalszahlen-rate: bitte eine zahl zwischen 1 und", max, "eingebn")
+print("Zufallszahlen-rate: bitte eine zahl zwischen 1 und", max, "eingebn")
 while(gewonnen == False):
     zahl = input("Welche Zahl? ")
     zahl = int(zahl)
-    if(zahl == zufallszahl):
+    versuche += 1
+    if(zahl == Zufallszahl):
         gewonnen = True
-    if ( zahl < zufallszahl):
-        print("-----")
-    if (zahl > zufallszahl):
-        print("+++++") 
+    if ( zahl < Zufallszahl):
+        print("mehr")
+        
+    if (zahl > Zufallszahl):
+        print("weniger") 
+      
 
 
-print("Bravo - du hast die Zahl in", versuche, "Versuchen erraten")
+print("Bravo - Sie haben die Zahl in", versuche, "Versuchen erraten")
